658fa6d782804eb3800d8a042dda76c38ffd75c5
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-07-29 07:57:31 +0800

    Initial test practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import type { FC } from 'react'
 
 const App: FC = () => {
+  const [contacts, setContacts] = useState<Array<string>>([])
+  const [contactName, setContactName] = useState<string | undefined>()
+
+  const onInputChange = useCallback(
+    ({ target: { value } }) => {
+      setContactName(value)
+    },
+    [setContactName],
+  )
+
+  const onAddContact = useCallback(() => {}, [])
+
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <p>Hello Booky</p>
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="flex flex-row items-center justify-around">
+        <input
+          className="p-2 mx-4 border-2 border-gray-400 rounded-md"
+          type="text"
+          placeholder="contact"
+          onChange={onInputChange}
+          value={contactName}
+        />
+        <button className="w-24 bg-blue-400 h-11 rounded-md">
+          <p className="text-white">Add</p>
+        </button>
+        <button className="w-24 m-2 bg-blue-400 h-11 rounded-md">
+          <p className="text-white">Remove</p>
+        </button>
+        <button className="w-24 bg-blue-400 h-11 rounded-md">
+          <p className="text-white">Clear</p>
+        </button>
+      </div>
+      <div className="flex flex-col mt-4">
+        <p>List of Contacts</p>
+        {contacts.map((c) => (
+          <p>{c}</p>
+        ))}
+      </div>
     </div>
   )
 }
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import App from '../src/App'
+import App from '../App'
 
-test('renders learn react link', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+describe('UI components', () => {
+  beforeAll(() => {
+    render(<App />)
+  })
+
+  it('should render Add button', () => {
+    const addButton = screen.getByText(/Add/i)
+    expect(addButton).toBeInTheDocument()
+  })
 })
