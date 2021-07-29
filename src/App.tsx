154a91c25d6ac41102cd1b9df9a69615fdfe87b1
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-07-29 10:16:46 +0800

    Presentation initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,13 @@ import React, { useState, useCallback } from 'react'
 import type { FC } from 'react'
 
 const App: FC = () => {
-  const [contacts, setContacts] = useState<Array<string>>([])
-  const [contactName, setContactName] = useState<string>('')
-
-  const onInputChange = useCallback(({ target: { value } }) => {
-    setContactName(value)
-  }, [])
-
-  const onAddContact = useCallback(() => {
-    setContacts((state) => [...state, contactName] as Array<string>)
-  }, [contactName])
-
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen">
-      <div className="flex flex-row items-center justify-around">
-        <input
-          className="p-2 mx-4 border-2 border-gray-400 rounded-md"
-          type="text"
-          placeholder="Contact name"
-          value={contactName}
-          onChange={onInputChange}
-        />
-        <button
-          onClick={onAddContact}
-          className="w-24 bg-blue-400 h-11 rounded-md"
-        >
-          <p className="text-white">Add</p>
-        </button>
-        <button className="w-24 m-2 bg-blue-400 h-11 rounded-md">
-          <p className="text-white">Remove</p>
-        </button>
-        <button className="w-24 bg-blue-400 h-11 rounded-md">
-          <p className="text-white">Clear</p>
-        </button>
-      </div>
-      <div className="flex flex-col mt-4">
-        <p>List of Contacts</p>
-        {contacts.map((c) => (
-          <p key={c}>{c}</p>
-        ))}
-      </div>
+      <p className="text-3xl animate-bounce">Hello Eggplant 🍆</p>
+      <p className="mt-4 text-xl">Welcome to our first TDD Session</p>
+      <p className="text-md">
+        Edit <code>src/App.tsx</code> and save to reload
+      </p>
     </div>
   )
 }
--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,39 +1,9 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import App from '../App'
 
-describe('UI components', () => {
-  beforeAll(() => {
-    render(<App />)
-  })
-
-  it('should render Add button', () => {
-    const addButton = screen.getByText(/Add/i)
-    expect(addButton).toBeInTheDocument()
-  })
-})
-
-describe('UI functionality', () => {
-  // Arrange
-  // Act
-  // Assert
-
-  describe('given that a user inputs a name and clicks the Add button', () => {
-    it('should add the inputted name to the list', async () => {
-      // Arrange
-      render(<App />)
-      const inputField = screen.getByRole('textbox')
-      const addButton = screen.getByRole('button', { name: 'Add' })
-
-      // Act
-      userEvent.type(inputField, 'Jett Andres')
-      userEvent.click(addButton)
-
-      // Assert
-      const contactList = await screen.findByText('Jett Andres')
-
-      expect(contactList).toBeInTheDocument()
-    })
-  })
+it('should render App', () => {
+  render(<App />)
+  const title = screen.getByText('Hello Eggplant 🍆')
+  expect(title).toBeDefined()
 })
